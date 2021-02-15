a7070256d91eaf6671a63df6f61c12ea04f2ae76
frederikpil <frpi@itu.dk>
2021-02-15 09:25:44 +0100

    final

---
+++ itu_vid_it/android/app/src/main/kotlin/bachelor/app/ituvidit/MainActivity.kt
@@ -0,0 +1,6 @@
+package bachelor.app.ituvidit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- vid_it/android/app/src/main/kotlin/itu/vid_it/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package itu.vid_it
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
