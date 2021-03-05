c69e9588120b0ac1a7af10f46f19818a0c792e48
frpi <Uub24atdITU>
2021-03-05 09:42:46 +0100

    BLE_server now receives left and right data when button is taped

--- Arduino/BLE_server/BLE_server.ino
+++ Arduino/BLE_server/BLE_server.ino
@@ -9,15 +9,38 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <AccelStepper.h>
+
 
 #define SERVICE_UUID        "ea411899-d14c-45d5-81f0-ce96b217c64a"
 #define CHARACTERISTIC_UUID "91235981-23ee-4bca-b7b2-2aec7d075438"
+#define CHARACTERISTIC_UUID_RX "52e6a8f9-5688-4d81-b1ad-ece87b095e52"
 
 const int ledPin = 2;
 BLEServer *VidItServer;
 BLEService *VidItService;
 BLECharacteristic *VidItCharacteristic;
 BLEAdvertising *VidItAdvertising;
+
+// defines pins numbers
+// 1st motor
+const int stepPin_1 = 33; 
+const int dirPin_1 = 32; 
+const int MS1_1 = 27; 
+const int MS2_1 = 26; 
+const int MS3_1 = 25; 
+
+// 2nd motor
+const int stepPin_2 = 22; 
+const int dirPin_2 = 23;
+const int MS1_2 = 18; 
+const int MS2_2 = 19; 
+const int MS3_2 = 21; 
+
+// Define motor interface type
+#define motorInterfaceType 1
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -28,6 +51,8 @@ void setup() {
   BLEDevice::init("VidItESP32");
   //Creating the BLE device as a BLE server
   VidItServer = BLEDevice::createServer();
+
+  
   //Creating BLE service with the service UUID
   VidItService = VidItServer->createService(SERVICE_UUID);
   //Creating BLE characteristic with UUID wit properties read and write.
@@ -36,6 +61,7 @@ void setup() {
                                          BLECharacteristic::PROPERTY_READ |
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
+                                       
   //The setValue should be changed in the future to display the state
   VidItCharacteristic->setValue("LOL");
   VidItService->start();
@@ -49,35 +75,55 @@ void setup() {
   BLEDevice::startAdvertising();
   
   Serial.println("READY TO BE PAIRED");
+
+  
+  pinMode(stepPin_1,OUTPUT); 
+  pinMode(dirPin_1,OUTPUT);
+  digitalWrite(stepPin_1,HIGH);
+  
 }
 
 
 int connectedUsers = 0;
 
 void loop() {
+  
   connectedUsers = VidItServer->getConnectedCount();
+  
   if(connectedUsers > 0){
+    
     digitalWrite(ledPin, HIGH);
-    //Debug statement - printing in console.
     Serial.println(VidItCharacteristic->getValue().c_str());
-    
-    
-    
-  }else
-  {
-    digitalWrite(ledPin, LOW);
-   
+    //Debug statement - printing in console.
+    if (VidItCharacteristic->getValue()== "Right"){                   
+       //Counter clockwise
+       digitalWrite(dirPin_1,HIGH); // Enables the motor to move in a particular direction      
+          while(VidItCharacteristic->getValue()== "Right"){
+            digitalWrite(stepPin_1,HIGH); 
+          delayMicroseconds(500); 
+          digitalWrite(stepPin_1,LOW); 
+          delayMicroseconds(500); 
+            }
+          
+        delay(10); // One second delay
+      }
+      else if (VidItCharacteristic->getValue()== "Left"){
+          //Clockwise
+          digitalWrite(dirPin_1,LOW); //Changes the rotations direction
+          // Makes 400 pulses for making two full cycle rotation
+          while(VidItCharacteristic->getValue()== "Left"){
+            digitalWrite(stepPin_1,HIGH);
+            delayMicroseconds(500);
+            digitalWrite(stepPin_1,LOW);
+            delayMicroseconds(500);
   }
-
-  
-  
-
-
-
-
-
-
-  
-  
-  
+  delay(10);
+        }
+        else{
+          //Do nothing
+          }   
+  }
+  else{
+    digitalWrite(ledPin, LOW);
+  }
 }
