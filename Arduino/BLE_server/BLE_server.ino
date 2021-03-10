83f5276ff0a4422bdbe522271e5d80c4214fb017
frpi <Uub24atdITU>
2021-03-10 13:35:30 +0100

    upload af up og ned

--- Arduino/BLE_server/BLE_server.ino
+++ Arduino/BLE_server/BLE_server.ino
@@ -16,7 +16,7 @@
 #define CHARACTERISTIC_UUID "91235981-23ee-4bca-b7b2-2aec7d075438"
 #define CHARACTERISTIC_UUID_RX "52e6a8f9-5688-4d81-b1ad-ece87b095e52"
 
-const int ledPin = 2;
+const int ledPin = 3;
 BLEServer *VidItServer;
 BLEService *VidItService;
 BLECharacteristic *VidItCharacteristic;
@@ -32,7 +32,7 @@ const int MS3_1 = 25;
 
 // 2nd motor
 const int stepPin_2 = 22; 
-const int dirPin_2 = 23;
+const int dirPin_2 = 2;
 const int MS1_2 = 18; 
 const int MS2_2 = 19; 
 const int MS3_2 = 21; 
@@ -97,20 +97,16 @@ void loop() {
     //Debug statement - printing in console.
     
     if (VidItCharacteristic->getValue()== "Right"){
-      //Serial.println("Gooo right");                   
        //Counter clockwise
        digitalWrite(dirPin_1,HIGH); // Enables the motor to move in a particular direction      
-         // while(VidItCharacteristic->getValue()== "Right"){
             digitalWrite(stepPin_1,HIGH); 
           delayMicroseconds(500); 
           digitalWrite(stepPin_1,LOW); 
           delayMicroseconds(500); 
-           // }
           
-        delay(10); // One second delay
+        delay(10); 
       }
       else if (VidItCharacteristic->getValue()== "Left"){
-      //Serial.println("Gooo left");  
           //Clockwise
           digitalWrite(dirPin_1,LOW); //Changes the rotations direction
           // Makes 400 pulses for making two full cycle rotation
@@ -119,9 +115,24 @@ void loop() {
             delayMicroseconds(500);
             digitalWrite(stepPin_1,LOW);
             delayMicroseconds(500);
-  //}
-  delay(10);
+  delay(10); 
         }
+        else if(VidItCharacteristic->getValue()== "Up"){
+          digitalWrite(dirPin_2,HIGH); // Enables the motor to move in a particular direction      
+            digitalWrite(stepPin_2,HIGH); 
+          delayMicroseconds(500); 
+          digitalWrite(stepPin_2,LOW); 
+          delayMicroseconds(500); 
+          delay(10);
+          }
+          else if(VidItCharacteristic->getValue()== "Down"){
+          digitalWrite(dirPin_2,LOW); // Enables the motor to move in a particular direction      
+            digitalWrite(stepPin_2,HIGH); 
+          delayMicroseconds(500); 
+          digitalWrite(stepPin_2,LOW); 
+          delayMicroseconds(500); 
+          delay(10);
+          }
         else{
           //Do nothing
           //Serial.println("FUCKING HOLD BITHC");  
