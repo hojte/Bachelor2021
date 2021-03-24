5fbd2968abc6805d073dce45bc9e3023e795aa01
Mathias Højte Olsen <mhoo@itu.dk>
2021-03-24 15:05:44 +0100

    move static analysis into drawer + linting and wording

--- itu_vid_it/lib/customDrawer.dart
+++ itu_vid_it/lib/customDrawer.dart
@@ -2,11 +2,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:ituvidit/static.dart';
+
+Widget renderDetectImageButton(BuildContext context){
+  return ElevatedButton(
+    child: Text("Detect in an Image"),
+    onPressed: () {
+      Navigator.push(context, MaterialPageRoute(
+        builder: (context) => StaticImage(),
+      ),
+      );
+    },
+  );
+}
 
 class CustomDrawer extends HookWidget{
-  void Function(bool boolValue) setValue;
-  bool debugModeValue;
-  CustomDrawer(void Function(bool boolValue) this.setValue, this.debugModeValue);
+  final Function(bool boolValue) setValue;
+  final bool debugModeValue;
+  CustomDrawer(this.setValue, this.debugModeValue);
 
   @override
   Widget build(BuildContext context) {
@@ -16,15 +29,16 @@ class CustomDrawer extends HookWidget{
         children: [
           SwitchListTile(
               activeColor: Colors.green,
-              title: Text("Debug mode"),
-              subtitle: Text("Do you want to display debug mode?"),
+              title: Text("Tracking Boxes"),
+              subtitle: Text("Do you want to display the tracking boxes?"),
               value: switchValue.value,
               onChanged: (bool newValue){
                 switchValue.value = !switchValue.value;
                 newValue = switchValue.value;
                 setValue(switchValue.value);
           }
-          )
+          ),
+          renderDetectImageButton(context),
         ],
       ),
     );
